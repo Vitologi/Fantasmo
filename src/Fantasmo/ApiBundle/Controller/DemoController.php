0296c56ed287c971f40a7e9680cd20849e1bee82
Spektr <spektrasm@gmail.com>
2017-01-07 22:23:38 +0300

    put some api doc

--- src/Fantasmo/ApiBundle/Controller/DemoController.php
+++ src/Fantasmo/ApiBundle/Controller/DemoController.php
@@ -5,9 +5,25 @@
 namespace Fantasmo\ApiBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class DemoController extends FOSRestController
 {
+
+    /**
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method",
+     *  filters={
+     *      {"name"="a-filter", "dataType"="integer"},
+     *      {"name"="another-filter", "dataType"="string", "pattern"="(foo|bar) ASC|DESC"}
+     *  }
+     * )
+     */
     public function getDemosAction()
     {
         $data = array("hello" => "world");
